382451cfc5629d2682c0508fa13b0e7300a2f13f
Luiz Victor (Smith) <luiz.victor100@hotmail.com>
2021-04-06 00:54:49 -0300

    finished the challenge

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userEmail = this.usersRepository.findByEmail(email);
+    if (userEmail) {
+      throw new Error("Email already exists!");
+    }
+    const user = this.usersRepository.create({ email, name });
+
+    return user;
   }
 }
 
